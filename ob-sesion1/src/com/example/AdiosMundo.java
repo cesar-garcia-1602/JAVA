60c71c4883c13fb257c6f8eae6ec6cf776764ab0
Cesar Daniel Garcia Ramirez <cesardanielgarciaramirez@hotmail.com>
2023-08-23 15:31:47 -0500

    Comentarios

---
+++ ob-sesion1/src/com/example/AdiosMundo.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class AdiosMundo {
+    public static void main(String[] args) {
+
+        // esto es un comentario
+        System.out.println("Adios mundo"); // esto es otro comentario
+        /*
+        Esto es un comentario multiline:
+        1. Crear una función.
+        2. Crear variable tipo numericas.
+        3. Utilizar la funcion suma para calcular la adición de dos numeros.
+        */
+
+    }
+}
