677643de1edab8add65236ea1f14a6ad6a62ab9f
Cesar Daniel Garcia Ramirez <cesardanielgarciaramirez@hotmail.com>
2023-08-25 23:04:00 -0500

    Variables

--- HolaMundo/src/com/HolaMundo/HolaMundo.java
+++ HolaMundo/src/com/HolaMundo/HolaMundo.java
@@ -1,7 +1,18 @@
 package com.HolaMundo;
 
 public class HolaMundo {
+
+    /**
+     *
+     * @param args main
+     */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo"); //Esto es un Hola Mundo
+        /*
+        Esto
+        es
+        un
+        comentario
+        */
     }
 }
---
+++ Variables/src/com/Variable/Variable.java
@@ -0,0 +1,20 @@
+package com.Variable;
+
+public class Variable {
+    public static void main(String[] args) {
+        // byte, short, int, long
+        byte entero = 12; //8bits -128 / 127
+        System.out.println("Numero entero: " + entero);
+        // float
+        float decimal = 3.45F; // Siempre se debe poner una f
+        System.out.println("Numero decimal: " + decimal);
+        double decimal1 = 3.4525265;
+        System.out.println("Numero decimal: " + decimal1);
+        // char
+        char caracter = 'a';
+        System.out.println("Caracter: " + caracter);
+        // Boolean
+        boolean decision = true;
+        System.out.println("La decisión es: " + decision);
+    }
+}
