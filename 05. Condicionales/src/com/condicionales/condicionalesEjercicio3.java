fb53cda65bdc18281dd5b2c4020cbd3512779863
Cesar Daniel Garcia Ramirez <cesardanielgarciaramirez@hotmail.com>
2023-08-30 10:38:31 -0500

    Condicionales - Ejercicio 3

--- 05. Condicionales/src/com/condicionales/condicionalesEjercicio3.java
+++ 05. Condicionales/src/com/condicionales/condicionalesEjercicio3.java
@@ -1,4 +1,19 @@
 package com.condicionales;
 
+import javax.swing.*;
+
 public class condicionalesEjercicio3 {
+    public static void main(String[] args) {
+        /*
+        Hacer un programa que lea un caracter por teclado y compruebe si es una letra mayuscula
+        */
+        char letra;
+        letra = JOptionPane.showInputDialog("Digite una letra: ").charAt(0);
+        if (Character.isUpperCase(letra)){
+            JOptionPane.showMessageDialog(null, "Es una letra mayuscula");
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Es una letra minuscula");
+        }
+    }
 }
