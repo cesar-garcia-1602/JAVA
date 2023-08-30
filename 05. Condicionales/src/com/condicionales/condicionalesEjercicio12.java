1e87162ca61286d2624d1b8ac2e9ae7237e41f3f
Cesar Daniel Garcia Ramirez <cesardanielgarciaramirez@hotmail.com>
2023-08-30 16:12:01 -0500

    Condicionales - Ejercicio 12

--- 05. Condicionales/src/com/condicionales/condicionalesEjercicio12.java
+++ 05. Condicionales/src/com/condicionales/condicionalesEjercicio12.java
@@ -1,4 +1,45 @@
 package com.condicionales;
 
+import javax.swing.*;
+
 public class condicionalesEjercicio12 {
+    public static void main(String[] args) {
+        /*
+        Pedir una nota entre 0 y 10 y mostrarla de la forma:
+        - Insuficiente
+        - Suficiente
+        - Bien
+        - Notable
+        - Sobresaliente
+        */
+        int nota;
+
+        nota= Integer.parseInt(JOptionPane.showInputDialog("Ingrese su nota del 0-10: "));
+
+        switch(nota) {
+            case 0:
+            case 1:
+            case 2:
+                JOptionPane.showMessageDialog(null, "Nota insuficiente.");
+                break;
+            case 3:
+            case 4:
+                JOptionPane.showMessageDialog(null,"Nota suficiente.");
+                break;
+            case 5:
+            case 6:
+                JOptionPane.showMessageDialog(null,"Nota bien.");
+                break;
+            case 7:
+            case 8:
+                JOptionPane.showMessageDialog(null,"Nota Notable.");
+                break;
+            case 9:
+            case 10:
+                JOptionPane.showMessageDialog(null,"Nota Sobresaliente.");
+                break;
+            default:
+                JOptionPane.showMessageDialog(null,"La nota tiene que ser de 0-10.");
+        }
+    }
 }
