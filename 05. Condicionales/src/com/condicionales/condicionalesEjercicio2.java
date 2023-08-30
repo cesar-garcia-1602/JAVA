d98a32c6c0569a91ed91131025c53fdced78695c
Cesar Daniel Garcia Ramirez <cesardanielgarciaramirez@hotmail.com>
2023-08-30 09:59:24 -0500

    Condicionales - Ejercicio 2

--- 05. Condicionales/src/com/condicionales/condicionalesEjercicio2.java
+++ 05. Condicionales/src/com/condicionales/condicionalesEjercicio2.java
@@ -1,4 +1,20 @@
 package com.condicionales;
 
+import javax.swing.*;
+
 public class condicionalesEjercicio2 {
+    public static void main(String[] args) {
+        int num1, num2;
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite el primer numero: "));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo numero: "));
+        if (num1 < num2){
+            JOptionPane.showMessageDialog(null, "El numero " + num1 + " es menor que el numero " + num2);
+        }
+        else if (num1 > num2){
+            JOptionPane.showMessageDialog(null, "El numero " + num1 + " es mayor que el numero" + num2);
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Los dos numeros son iguales");
+        }
+    }
 }
